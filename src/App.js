7f178ffc24ee4bf0816d03976d062229f492f49f
abdulkyume <abbirtak@gmail.com>
2022-08-08 00:49:17 +0600

    core concepts

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   var person = {
     name: "Abdul",
     job: "fresher"
-  }
+  };
+  const products = [{name:'Photoshop', details:'$90.00'}]
   return (
     <div className="App">
         <p>My first react {person.name}</p>
@@ -12,11 +13,30 @@ function App() {
         <Person name="Abdul" job="student"></Person>
         <Person name="Kyume" job="student"></Person>
         <Person name="Takbir" job="student"></Person>
+        <Product name={products[0].name} details={products[0].details}></Product>
     </div>
     
   );
 }
 
+
+function Product(prodcts){
+  const productStyle={
+    border:'1px solid grey',
+    borderRadius: '5px',
+    backgroundColor:'lightgray',
+    float:'left',
+    height:'200px',
+    width:'200px',
+  }  
+  return (
+    <div style={productStyle}>
+      <h2>name: {prodcts.name}</h2>
+      <h1>{prodcts.details}</h1>
+      <button>Buy Now</button>
+    </div>
+  )
+}
 
 function Person(variable){
     return <div>
