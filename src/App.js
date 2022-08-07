87410f9f8efd9503d447c622ff6c8b0463906fbb
abdulkyume <abbirtak@gmail.com>
2022-08-07 23:39:54 +0600

    2nd comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  var person = {
+    name: "Abdul",
+    job: "fresher"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>My first react {person.name}</p>
+        <p>My first react {person.job}</p>
+        <Person name="Abdul" job="student"></Person>
+        <Person name="Kyume" job="student"></Person>
+        <Person name="Takbir" job="student"></Person>
     </div>
+    
   );
+}
+
+
+function Person(variable){
+    return <div>
+      <h1 style={{color:'red',backgroundColor:'lightblue'}}>name: {variable.name}</h1>
+      <h2 style={{color:'lightblue',backgroundColor:'red'}}>Friend: {variable.job}</h2>
+    </div>;
 }
 
 export default App;
